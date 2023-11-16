848e9668f51bd4f2a090fa52bbddb2494d0e8e09
Eizaku Asai <vmr4ku@virginia.edu>
2023-11-16 16:41:11 -0500

    1

--- data_interpreter.py
+++ data_interpreter.py
@@ -16,14 +16,19 @@ class data_interpreter():
         self.drop_tables()
         self.intialize_database()
         self.fill_database()
+
         self.determine_date_added_bounds()
-        self.build_plot_data("country", "date_added", 10, "Director Movie Frequency", labels=["Year Added", "Movies"])
+
+        self.build_plot_data("rating", "date_added", 10, "Rating in US Frequency", labels=["Year Added", "Number of Movies"], filter={"country": "United States"})
+        self.build_plot_data("rating", "date_added", 10, "Rating in US Frequency", labels=["Year Added", "Number of Movies"])
         
 
-    def build_plot_data(self, x, y, points, title, labels):
-        query = '''select * from %s''' % (x)
-        frequent_directors = self.data_base_query(query)
-        parsed_data = self.parse_frequency_data(frequent_directors, 2, 1, y)
+    def build_plot_data(self, x:str, y:str, points:int, title:str, labels:list, filter:dict=None):
+        query = self.build_filtered_query(x, y, filter)
+        print(query)
+        frequent = self.database_query(query)
+        parsed_data = self.parse_frequency_data(frequent, True)
+        # print(parsed_data)
         for point in range(len(parsed_data)):
             parsed_data[point] = (parsed_data[point][0], self.fill_in_blank_year_data(parsed_data[point][1]), parsed_data[point][2])
         plot_data = self.get_graph_data(parsed_data[:points])
@@ -36,6 +41,28 @@ class data_interpreter():
                 full_data.append((year, 0))
         full_data = sorted(full_data, key=lambda x: x[0])
         return [[i[0] for i in full_data],[i[1] for i in full_data]]
+
+    def build_filtered_query(self, x:str, y:str, filter:dict=None):
+        table_name = x
+        x_data = x
+        if x in self.separate_tables:
+            x_data = x + "NAME"
+        else:
+            table_name = "MOVIE"
+        query = '''SELECT \"%s\", \"%s\" FROM \"%s\"''' % (x_data, y, table_name)
+        if not y in self.separate_tables and x in self.separate_tables:
+            query += " JOIN MOVIE ON MOVIE.ID=\"%s\".MOVIE_ID" % (x)
+        if filter != None and len(filter.keys()) > 0:
+            for key in filter.keys():
+                condition = "\"%s\"=\"%s\"" % (key, filter[key])
+                if key in self.separate_tables and x in self.separate_tables:
+                    condition = "\"%s\"=\"%s\" AND \"%s\".MOVIE_ID=\"%s\".MOVIE_ID" % (key + "NAME", filter[key], table_name, key)
+                elif key in self.separate_tables:
+                    condition = "\"%s\"=\"%s\" AND \"%s\".ID=\"%s\".MOVIE_ID" % (key + "NAME", filter[key], table_name, key)
+                query += " JOIN \"%s\" ON %s" % (key, condition)
+        query += ";"
+        return query
+
 
     def determine_date_added_bounds(self):
         max = float('-inf')
@@ -57,60 +84,52 @@ class data_interpreter():
         y_labels = [i[0] for i in data]
         return x_data, y_data, y_labels
 
-    def parse_frequency_data(self, data:list, data_index:int, fk_index:int, compare:str=None):
+    def parse_frequency_data(self, data:list, is_date_added:bool=False):
         freq_dict = {}
-        if compare != None:
-            for point in data:
-                result_list = self.data_base_query("SELECT %s FROM MOVIE WHERE ID=%d" % (compare, point[fk_index]))
-                result = result_list[0][0]
-                try:
-                    if compare == "date_added":
-                        result = int(result[-4:])
-                    if not point[data_index] in freq_dict.keys():
-                        freq_dict[point[data_index]] = {}
-                    if not result in freq_dict[point[data_index]].keys():
-                        freq_dict[point[data_index]][result] = 0
-                    freq_dict[point[data_index]][result] += 1
-                except ValueError:
-                    pass
-            return_list = []            
-            for key in freq_dict.keys():
-                append_list = [[],[]]
-                total = 0
-                for i in sorted(freq_dict[key]):
-                    append_list[0].append(i)
-                    append_list[1].append(freq_dict[key][i])
-                    total += freq_dict[key][i]
-                freq_dict[key] = append_list
-                return_list.append((key, freq_dict[key], total))
-            return_list = sorted(return_list, key=lambda index: -index[2])
-        else:
-            for point in data:
-                if not point[data_index] in freq_dict.keys():
-                    freq_dict[point[data_index]] = 0
-                freq_dict[point[data_index]] += 1
-            return_list = [(i, freq_dict[i]) for i in sorted(freq_dict, key=lambda name: -freq_dict[name])]
+        for point in data:
+            y_field = point[1]
+            try:
+                if is_date_added:
+                    y_field = int(y_field[-4:])
+                if not point[0] in freq_dict.keys():
+                    freq_dict[point[0]] = {}
+                if not y_field in freq_dict[point[0]].keys():
+                    freq_dict[point[0]][y_field] = 0
+                freq_dict[point[0]][y_field] += 1
+            except ValueError:
+                pass
+        return_list = []            
+        for key in freq_dict.keys():
+            append_list = [[],[]]
+            total = 0
+            for i in sorted(freq_dict[key]):
+                append_list[0].append(i)
+                append_list[1].append(freq_dict[key][i])
+                total += freq_dict[key][i]
+            freq_dict[key] = append_list
+            return_list.append((key, freq_dict[key], total))
+        return_list = sorted(return_list, key=lambda index: -index[2])
         return return_list
 
     def fill_database(self):
         connection = sqlite3.connect(self.sqlite_table)
         execute_string = self.build_execute_string()
-        self.data_base_execute_query(execute_string)
+        self.database_execute_query(execute_string)
 
         for row in range(len(self.table)):
             current = self.table.iloc[row]
             for column in self.separate_tables:
-                foreign_key = connection.execute("SELECT * FROM MOVIE WHERE show_id=\"%s\"" % (current.get("show_id"))).fetchall()[0][0]
+                foreign_key = connection.execute("SELECT * FROM \"MOVIE\" WHERE show_id=\"%s\"" % (current.get("show_id"))).fetchall()[0][0]
                 names = str(current.get(column)).replace("'", "''").split(",")
                 for name in names:
                     if name != "nan":
                         name = name.strip()
-                        execute_string = '''INSERT INTO %s (MOVIE_ID, NAME) VALUES ('%s', '%s')''' % (column, foreign_key, name)
+                        execute_string = '''INSERT INTO %s (\"MOVIE_ID\", \"%sNAME\") VALUES ('%s', '%s')''' % (column, column, foreign_key, name)
                         connection.execute(execute_string)
         self.close_database(connection)
 
     def build_execute_string(self):
-        execute_string = "INSERT INTO MOVIE ("
+        execute_string = "INSERT INTO \"MOVIE\" ("
         for column in self.table.columns:
             if not column in self.separate_tables:
                 execute_string += column + ","
@@ -133,18 +152,18 @@ class data_interpreter():
 
     def drop_tables(self):
         try:
-            self.data_base_execute_query("DROP TABLE MOVIE;")
+            self.database_execute_query("DROP TABLE \"MOVIE\";")
         except sqlite3.OperationalError as e:
             print(e)
         for sub_table in self.separate_tables:
             try:
-                self.data_base_execute_query("DROP TABLE %s;" % (sub_table))
+                self.database_execute_query("DROP TABLE %s;" % (sub_table))
             except sqlite3.OperationalError as e:
                 print(e)
 
     def intialize_database(self):
         try:
-            execute_string = '''CREATE TABLE MOVIE (
+            execute_string = '''CREATE TABLE \"MOVIE\" (
                 ID INTEGER PRIMARY KEY,'''
             for column in self.table.columns:
                 if not column in self.separate_tables:
@@ -155,31 +174,31 @@ class data_interpreter():
                         execute_string += '''
                         %s VARCHAR (1000),''' % (column)
             execute_string = execute_string[:-1] + ");"
-            self.data_base_execute_query(execute_string)
+            self.database_execute_query(execute_string)
         except sqlite3.OperationalError as e:
             print(e)
 
         for sub_table in self.separate_tables:
             try:
-                self.data_base_execute_query(
-                    '''CREATE TABLE %s(
+                self.database_execute_query(
+                    '''CREATE TABLE \"%s\"(
                         ID INTEGER PRIMARY KEY,
                         MOVIE_ID INT,
-                        NAME VARCHAR(100) NOT NULL,
+                        %sNAME VARCHAR(100) NOT NULL,
                         FOREIGN KEY (MOVIE_ID) REFERENCES MOVIE (ID)
                         )
-                        ''' % (sub_table)
+                        ''' % (sub_table, sub_table)
             ) 
             except sqlite3.OperationalError as e:
                 print(e)
         
-    def data_base_query(self, query:str):
+    def database_query(self, query:str):
         connection = sqlite3.connect(self.sqlite_table)
         data = connection.execute(query).fetchall()
         self.close_database(connection)
         return data
 
-    def data_base_execute_query(self, query:str):
+    def database_execute_query(self, query:str):
         connection = sqlite3.connect(self.sqlite_table)
         connection.execute(query)
         self.close_database(connection)
